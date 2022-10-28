c4239e1cd0f38f1bb8befcf75d9ea4c170e47745
elmeraa <eazadpour@usgs.gov>
2022-10-28 14:57:35 -0700

    includes save RDS for shapefiles to read back into the qmd to plot

--- 1_fetch.R
+++ 1_fetch.R
@@ -10,20 +10,20 @@ p1_targets <- list(
              format = 'file'),
   # st_read() watershed extents
   tar_target(p1_watershed_ex,
-             st_read(p1_watershed_ext_sf) %>% st_transform(crs=p1_proj)),
+             st_read(p1_watershed_ext_sf) %>% st_transform(crs=p1_proj) %>% saveRDS('1_fetch/out/watershed_ext_sf.rds')),
   # Read in saline lake
   tar_target(p1_saline_lakes_sf,
              '1_fetch/in/Vizlab-saline_lakes_spatial_data/saline_lakes/saline_lakes.shp',
              format = 'file'),
   # st_read() saline lakes
   tar_target(p1_saline_lakes,
-             st_read(p1_saline_lakes_sf) %>% st_transform(crs=p1_proj)),
+             st_read(p1_saline_lakes_sf) %>% st_transform(crs=p1_proj) %>% saveRDS('1_fetch/out/saline_lakes_sf.rds')),
   # Great Basin Dessert Band
   tar_target(p1_gbd_bnd_sf,
              '1_fetch/in/Vizlab-saline_lakes_spatial_data/GreatBasinDessertBnd_220721/GreatBasinDessertBnd.shp',
              format = 'file'),
   tar_target(p1_gbd_bnd,
-             st_read(p1_gbd_bnd_sf) %>% st_transform(crs=p1_proj) %>%  ms_simplify()),
+             st_read(p1_gbd_bnd_sf) %>% st_transform(crs=p1_proj) %>%  ms_simplify() %>% saveRDS('1_fetch/out/gbd_bnd.rds')),
  #  PAD/land ownership
   tar_target(p1_pad_sf,
              '1_fetch/in/Shapefiles_220721/PADUS_3_0VA.shp',
@@ -35,6 +35,6 @@ p1_targets <- list(
              '1_fetch/in/states_shp/statesp010g.shp',
              format = 'file'),
  tar_target(p1_states,
-            sf::st_read(p1_states_sf) %>% st_transform(crs=p1_proj) %>% ms_simplify())
+            sf::st_read(p1_states_sf) %>% st_transform(crs=p1_proj) %>% ms_simplify() %>% saveRDS('1_fetch/out/states.rds'))
  
 )
